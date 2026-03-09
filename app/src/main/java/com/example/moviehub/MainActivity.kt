767e49cb61ff0cd2e76a3a4550b02a91f3266cc8
Kirill Talashko <KirillTalashk@yandex.ru>
2026-03-09 17:51:30 +0300

    Setting up Gradle, creating the home screen

--- app/src/main/java/com/example/moviehub/MainActivity.kt
+++ app/src/main/java/com/example/moviehub/MainActivity.kt
@@ -4,44 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.main.ui.MainHomeScreen
 import com.example.moviehub.ui.theme.MovieHubTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieHubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MovieHubTheme(
+                darkTheme = true,
+                dynamicColor = false
+            ) {
+                MainHomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MovieHubTheme {
-        Greeting("Android")
     }
 }
